64d713ea6949311c1e88e2ca354896d076219dfe
Gerald <gm@gerald.host>
2022-04-16 22:52:19 +0100

    feat: more functions to array

--- src/test/Memory.t.sol
+++ src/test/Memory.t.sol
@@ -40,6 +40,58 @@ library Array {
       element := mload(add(self, mul(0x20, add(0x01, add(1, index))))) 
     }
   }
+
+  function min(Array memory self) internal pure returns (uint256 element) {
+    assembly {
+      let length := mload(self)
+      element := mload(add(self, 0x40))
+      for { let i := 0 } lt(i, length) { i := add(i, 1) }
+      {
+          let elm := mload(add(self, mul(0x20, add(0x01, add(1, i)))))
+          if lt(elm, element) {
+            element := elm
+          }
+      }
+    }
+  }
+
+  function max(Array memory self) internal pure returns (uint256 element) {
+    assembly {
+      let length := mload(self)
+      element := mload(add(self, 0x40))
+      for { let i := 0 } lt(i, length) { i := add(i, 1) }
+      {
+          let elm := mload(add(self, mul(0x20, add(0x01, add(1, i)))))
+          if gt(elm, element) {
+            element := elm
+          }
+      }
+    }
+  }
+
+  function sum(Array memory self) internal pure returns (uint256 sum) {
+    assembly {
+      let length := mload(self)
+      for { let i := 0 } lt(i, length) { i := add(i, 1) }
+      {
+          sum := add(sum, mload(add(self, mul(0x20, add(0x01, add(1, i))))))
+      }
+    }
+  }
+
+  function indexOf(Array memory self, uint256 element) internal pure returns (uint256 index) {
+    index = type(uint256).max;
+    assembly {
+      let length := mload(self)
+      for { let i := 0 } lt(i, length) { i := add(i, 1) }
+      {
+          let elm := mload(add(self, mul(0x20, add(0x01, add(1, i)))))
+          if eq(elm, element) {
+            index := i
+          }
+      }
+    }
+  }
 }
 
 contract Memory is DSTest {
@@ -48,7 +100,15 @@ contract Memory is DSTest {
   function testTing() external {
     Array.Array memory pa = Array.create(5);
     pa.push(125);
+    pa.push(126);
     assertEq(pa.get(0), 125);
     emit log_uint(pa.get(0));
+
+    emit log_uint(pa.sum());
+    emit log_uint(pa.min());
+    emit log_uint(pa.max());
+    emit log_uint(pa.indexOf(126));
+    emit log_uint(pa.indexOf(125));
+    emit log_uint(pa.indexOf(5));
   }
 }
